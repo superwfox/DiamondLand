eb95f4740d764bde712eac1841d0f4624e139e70
SUDARK <2963502563@qq.com>
2025-02-28 22:06:29 +0800

    sudark

---
+++ src/main/java/sudark/diamondland/DiamondLand.java
@@ -0,0 +1,17 @@
+package sudark.diamondland;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DiamondLand extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
